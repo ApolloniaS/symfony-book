eb835bfa0c92ea5ff0b6c2f2379fef313283ca8b
Apollonia Sorella <a.sorella@interface3.be>
2021-05-16 21:03:19 +0200

    squelette page livre

--- src/Controller/LivresController.php
+++ src/Controller/LivresController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Book;
 
 class LivresController extends AbstractController
 {
     #[Route('/livres', name: 'livres')]
     public function index(): Response
     {
-        return $this->render('livres/index.html.twig', [
-            'controller_name' => 'LivresController',
-        ]);
+
+        $em = $this->getDoctrine()->getManager();
+        $repLivre = $em->getRepository(Book::class);
+        $livre = $repLivre->findAll(); 
+
+        $vars = ['livres' => $livre];
+
+        return $this->render("livres/index.html.twig", $vars);
     }
 }
