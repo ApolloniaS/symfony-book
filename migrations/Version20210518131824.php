7da10492fea2e4df04a630176110fbb429363c11
Apollonia Sorella <a.sorella@interface3.be>
2021-05-18 15:43:53 +0200

    enregistrement DB ok (controller, pas vue)

--- migrations/Version20210518131824.php
+++ migrations/Version20210518131824.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210517081605 extends AbstractMigration
+final class Version20210518131824 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Entity\User;
+use App\Entity\UserBook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,12 +27,26 @@ class ProfilController extends AbstractController
         ]);
     }
 
-    #[Route('/profil/{titre}/{status}', name: 'updateProfil')]
+    #[Route('/profil/{id}/{titre}/{status}', name: 'updateProfil')]
     public function addStatus(Request $req)
     {
         
+    $idLivre = $req->get("id");
     $titreLivre = $req->get("titre");
     $statutLivre = $req->get("status");
+
+    $user = $this->getUser();
+    $em = $this->getDoctrine()->getManager();
+    $repLivre = $em->getRepository(Book::class);
+    $book = $repLivre->findOneBy(array("id" =>$idLivre));
+
+    //enregistrement dans la db
+    $userBook = new UserBook();
+    $userBook->setReadingStatus($statutLivre);
+    $userBook->setIdUser($user);
+    $userBook->setIdBook($book);
+    $em->persist($userBook);
+    $em->flush();
     
     $vars = ['toAdd' => [$titreLivre, $statutLivre]];
     return $this->render('profil/index.html.twig', $vars);
